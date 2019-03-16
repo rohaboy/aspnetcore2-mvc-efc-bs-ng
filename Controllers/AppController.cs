7921554ef5eb13e9bbd277b6e144fd6b54380e7a
LINUXHOST\nilesh <nileshaj@mlguptha.com>
2019-03-16 10:51:25 -0400

    Implemented MVC with basic validation, services and DI

---
+++ Controllers/AppController.cs
@@ -0,0 +1,52 @@
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+        public IActionResult Index()
+        {
+            //throw new Exception("Bad things happened");
+            return View();
+        }
+
+        [HttpGet("contact")] //contact is still discoverable by Tag Helper, but not by href url
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")] //contact is still discoverable by Tag Helper, but not by href url
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Send the email
+                _mailService.SendMessage("admin@DutchTreat.com",model.Subject,$"From:{model.Name} - {model.Email},Message:{model.Message}");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+           
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,14 +16,33 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IMailService, NullMailService>();
+            //add real mail service
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
+            
+            //app.UseDefaultFiles(); //This will stop serving all static default document files documen.html, index.htm, index.html etc.
             app.UseStaticFiles();
             app.UseNodeModules(env);
+
+            app.UseMvc(cfg=>
+            {
+                cfg.MapRoute("Default",
+                    "/{controller}/{action}/{id?}",
+                    new { controller = "App", Action = "Index" });
+            });
         }
     }
 }
