82eea288b3d0deb508a789761eb33e9b1f889a04
LINUXHOST\nilesh <nileshaj@mlguptha.com>
2019-03-19 07:15:51 -0400

    Added TypeScript Support

---
+++ wwwroot/ts/storecustomer.ts
@@ -0,0 +1,25 @@
+﻿/*export*/ class StoreCustomer {
+
+    public visits:number = 0; //this is how declaration is done
+    private ourName: string;
+
+    constructor(private firstName: string,private lastName:string) { //by declaring private automatic creation of private field is done and available in class
+        
+    }
+
+
+    //function
+    public showName() {
+        alert(this.firstName + " " + this.lastName);
+    }
+
+    //property must have setter and getter
+
+    set name(val) {
+        this.ourName = val;
+    }
+
+    get name() {
+        return this.ourName;
+    }
+}
